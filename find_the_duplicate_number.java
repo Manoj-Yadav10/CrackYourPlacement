6d8ebbea0cb1296d2f7891614e1ac9603c8a915c
Manoj-Yadav10 <157556792+Manoj-Yadav10@users.noreply.github.com>
2024-09-24 23:31:11 +0530

    Create find_the_duplicate_number.java

---
+++ find_the_duplicate_number.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int findDuplicate(int[] nums) {
+        int j=0;
+        int ans=0;
+        for(int i=1;i<nums.length;i++){
+          if(arr[j]==arr[i]){
+ans=arr[j];
+
+          }
+          j
+        }
+    }
+}
